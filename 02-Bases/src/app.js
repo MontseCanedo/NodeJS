6e3f077665364198b862297ae50d7e8a8d0869e2
MontseCanedo <montseykg@gmail.com>
2024-10-08 10:37:04 -0600

    loggers

--- 02-Bases/src/app.js
+++ 02-Bases/src/app.js
@@ -1,6 +1,5 @@
 const { buildMakePerson } = require('./js-fundation/05-factory');
 const { getId, getAge } = require('./plugins');
-
 // ! Referencia a la funcion factory y uso
 // const makePerson = buildMakePerson({getId, getAge})
 // const obj = { name: 'John', birthday: '1995-5-30'};
@@ -8,7 +7,11 @@ const { getId, getAge } = require('./plugins');
 // console.log(john);
 
 const getPokemonById = require('./js-fundation/06-promises');
+// getPokemonById(1).then( (pokemon) => console.log({pokemon}))
+// .catch( (err) => console.log({err}))
+// .finally ( () => console.log('Finalmente'));
 
-getPokemonById(1).then( (pokemon) => console.log({pokemon}))
-.catch( (err) => console.log({err}))
-.finally ( () => console.log('Finalmente'));
+const { buildLogger } = require('./plugins');
+const logger = buildLogger('app.js');
+logger.log('Hola mundo');
+logger.error('Esto es algo malo');
